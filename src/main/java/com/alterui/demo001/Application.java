53d5c583c79dd0509531fb6d53612562a0066520
liurui <liu.rui@oyohotels.cn>
2019-07-29 17:50:07 +0800

    test1. commit git to my first branch.

---
+++ src/main/java/com/alterui/demo001/Application.java
@@ -0,0 +1,11 @@
+package com.alterui.demo001;
+
+/**
+ * @author liurui
+ * @time 2019-07-29 17:48
+ */
+public class Application {
+    public static void main(String[] args) {
+        System.out.println("this is my first branch");
+    }
+}
